b692dae5d58cea26d660e95adefbb3fd713a4882
Jake Brehm <jake.m.brehm@gmail.com>
2025-02-20 16:45:14 -0500

    Update HTMLBuilder to use Attributes object; update tests

--- html_builder/core.py
+++ html_builder/core.py
@@ -5,7 +5,7 @@ Contains core functionality for the package.
 from abc import ABC, abstractmethod
 from typing import Any
 
-from .attributes import Classes, ClassesType
+from .attributes import Attributes, AttributesType, Classes, ClassesType
 
 
 class HTMLBuilder(ABC):
@@ -13,21 +13,18 @@ class HTMLBuilder(ABC):
 
     def __init__(
         self,
+        attributes: AttributesType | None = None,
         classes: ClassesType | None = None,
-        **kwargs: Any,
     ) -> None:
         """Initializes the HTMLTable object."""
 
         # Initialize instance variables
         self._elements: list[Any] = []
-        self._attributes: dict[str : Any | None] = {}
+        self._attributes = attributes if attributes else Attributes()
 
         # Set the classes if any were provided
-        if classes:
-            self.set_classes(classes)
-
-        # Update the attributes from the keyword arguments
-        self.update_attributes(kwargs)
+        if classes is not None:
+            self._attributes.classes = classes
 
     @abstractmethod
     def construct(self) -> str:
@@ -45,9 +42,14 @@ class HTMLBuilder(ABC):
         return self._elements
 
     @property
-    def attributes(self) -> dict[str : Any | None]:
+    def attributes(self) -> Attributes:
         """Gets the stored attributes."""
         return self._attributes
+
+    @property
+    def classes(self) -> Classes | None:
+        """Gets the stored classes."""
+        return self._attributes.classes
 
     def add_element(self, element: Any) -> None:
         """Adds an element to the HTML object."""
@@ -73,95 +75,96 @@ class HTMLBuilder(ABC):
         """Clears the classes of the HTML object."""
         self._elements.clear()
 
-    def set_attribute(
-        self,
-        attribute: str | dict[str:Any],  # TODO: Check type hint
-        value: Any | None = None,
-    ) -> None:
-        """Set an attributes of the HTML object."""
+    # def set_attribute( # TODO: Remove block
+    #     self,
+    #     attribute: str | dict[str:Any],
+    #     value: Any | None = None,
+    # ) -> None:
+    #     """Set an attributes of the HTML object."""
 
-        # Check if the first input is a dictionary or an attribute name
-        if isinstance(attribute, dict) and value is None:
-            self.update_attributes(attribute)
-        elif isinstance(attribute, str) and value is not None:
-            self.update_attributes({attribute: value})
-        else:
-            raise TypeError(
-                f"{self.set_attribute.__name__} accepts either a single "
-                "dictionary argument, or a string and its corresponding "
-                "value as two arguments."
-            )
+    #     # Check if the first input is a dictionary or an attribute name
+    #     if isinstance(attribute, dict) and value is None:
+    #         self.update_attributes(attribute)
+    #     elif isinstance(attribute, str) and value is not None:
+    #         self.update_attributes({attribute: value})
+    #     else:
+    #         raise TypeError(
+    #             f"{self.set_attribute.__name__} accepts either a single "
+    #             "dictionary argument, or a string and its corresponding "
+    #             "value as two arguments."
+    #         )
 
-    def set_attributes(self, attributes: dict[str : Any | None]) -> None:
-        """Sets attributes of the HTML object."""
-        self.update_attributes(attributes)
+    # def set_attributes(self, attributes: dict[str : Any | None]) -> None:
+    #     """Sets attributes of the HTML object."""
+    #     self.update_attributes(attributes)
 
-    def update_attributes(self, attributes: dict[str : Any | None]) -> None:
-        """Updates attributes of the HTML object."""
-        self._attributes.update(attributes)
+    # def update_attributes(self, attributes: dict[str : Any | None]) -> None:
+    #     """Updates attributes of the HTML object."""
+    #     self._attributes.update(attributes)
 
-    def pop_attribute(self, attribute: str) -> Any | None:
-        """Removes an attribute from the HTML object and returns it."""
-        return self._attributes.pop(attribute)
+    # def pop_attribute(self, attribute: str) -> Any | None:
+    #     """Removes an attribute from the HTML object and returns it."""
+    #     return self._attributes.pop(attribute)
 
-    def clear_attributes(self) -> None:
-        """Clears the attributes of the HTML object."""
-        self._attributes.clear()
+    # def clear_attributes(self) -> None:
+    #     """Clears the attributes of the HTML object."""
+    #     self._attributes.clear()
 
-    def add_class(self, name: str) -> None:
-        """Adds a class to the HTML object."""
-        if "class" not in self._attributes:
-            self._attributes["class"] = []
-        self._attributes["class"].add(name)
+    # def add_class(self, name: str) -> None:
+    #     """Adds a class to the HTML object."""
+    #     if "class" not in self._attributes:
+    #         self._attributes["class"] = []
+    #     self._attributes["class"].add(name)
 
-    def remove_class(self, name: str) -> None:
-        """Removes a class from the HTML object."""
-        if "class" in self._attributes:
-            self._attributes["class"].remove(name)
+    # def remove_class(self, name: str) -> None:
+    #     """Removes a class from the HTML object."""
+    #     if "class" in self._attributes:
+    #         self._attributes["class"].remove(name)
 
-    def set_classes(self, names: ClassesType) -> None:
-        """Adds a class to the HTML object."""
+    # def set_classes(self, names: ClassesType) -> None:
+    #     """Adds a class to the HTML object."""
 
-        # If the names are already a Classes object, return
-        if isinstance(names, Classes):
-            self._attributes["class"] = names
-            return
+    #     # If the names are already a Classes object, return
+    #     if isinstance(names, Classes):
+    #         self._attributes["class"] = names
+    #         return
 
-        # Convert the class name to a list if necessary
-        if isinstance(names, str):
-            names = [names]
+    #     # Convert the class name to a list if necessary
+    #     if isinstance(names, str):
+    #         names = [names]
 
-        # Verify that the list only contains strings
-        is_not_list = not isinstance(names, list)
-        is_not_all_strings = any(not isinstance(n, str) for n in names)
-        if is_not_list or is_not_all_strings:
-            raise TypeError(
-                f"{self.set_classes.__name__} only accepts a string or a "
-                "list of strings."
-            )
+    #     # Verify that the list only contains strings
+    #     is_not_list = not isinstance(names, list)
+    #     is_not_all_strings = any(not isinstance(n, str) for n in names)
+    #     if is_not_list or is_not_all_strings:
+    #         raise TypeError(
+    #             f"{self.set_classes.__name__} only accepts a string or a "
+    #             "list of strings."
+    #         )
 
-        # Clear the stored classes and add the provided ones
-        self._attributes["class"] = Classes(*names)
+    #     # Clear the stored classes and add the provided ones
+    #     self._attributes["class"] = Classes(*names)
 
-    def clear_classes(self) -> list[str] | None:
-        """Clears the classes of the HTML object.
+    # def clear_classes(self) -> list[str] | None:
+    #     """Clears the classes of the HTML object.
 
-        Returns the deleted classes.
-        """
-        return self._attributes.pop("class", None)
+    #     Returns the deleted classes.
+    #     """
+    #     return self._attributes.pop("class", None)
 
-    def attributes_to_string(self) -> str:
-        """Builds an attribute string from a dictionary of attributes."""
-        result = []
-        for key, value in self._attributes.items():
-            if (value is None) or (isinstance(value, bool) and value):
-                string = f"{key}"
-            elif value:
-                string = f"{key}='{value}'"
-            else:
-                continue
-            result.append(string)
-        return (" " + " ".join(result)) if result else ""
+    # def attributes_to_string(self) -> str:
+    #     """Builds an attribute string from a dictionary of attributes."""
+    #     return " " + str(self._attributes)
+    #     # result = []
+    #     # for key, value in self._attributes.attributes.items():
+    #     #     if (value is None) or (isinstance(value, bool) and value):
+    #     #         string = f"{key}"
+    #     #     elif value:
+    #     #         string = f"{key}='{value}'"
+    #     #     else:
+    #     #         continue
+    #     #     result.append(string)
+    #     # return (" " + " ".join(result)) if result else ""
 
     def __str__(self) -> str:
         """Gets a string version of the object."""
@@ -169,4 +172,4 @@ class HTMLBuilder(ABC):
 
     def __repr__(self) -> str:
         """Gets a string representation of the object."""
-        return str(self)  # TODO: Implement
+        return f"{self.__class__.__name__}(attributes={self._attributes!r})"
---
+++ test/core_test.py
@@ -0,0 +1,66 @@
+"""
+Contains tests for the core module.
+"""
+
+import pytest
+
+from html_builder.attributes import Attributes, Classes
+from html_builder.core import HTMLBuilder
+
+
+@pytest.fixture()
+def builder() -> HTMLBuilder:
+    """Creates a sample HTMLBuilder object with some attributes."""
+    attributes = {"id": "test", "disabled": True}
+    return HTMLBuilder(attributes=Attributes(attributes))
+
+
+def test_html_builder_init() -> None:
+    """Tests the initialization of the HTMLBuilder class."""
+
+    # Override the abstract methods
+    HTMLBuilder.__abstractmethods__ = set()
+
+    # Pass nothing
+    builder = HTMLBuilder()
+    assert builder.attributes == Attributes()
+    assert builder.classes is None
+
+    # Pass attributes
+    builder = HTMLBuilder(attributes=Attributes({"id": "test"}))
+    assert builder.attributes == Attributes({"id": "test"})
+    assert builder.classes is None
+
+    # Pass classes via attributes argument
+    expected_classes = Classes("class 1", "class2")
+    expected_attributes = Attributes({"class": "class-1 class2"})
+    builder = HTMLBuilder(attributes=expected_attributes)
+    assert builder.attributes == expected_attributes
+    assert builder.attributes.classes == expected_classes
+    assert builder.classes == expected_classes
+
+    # Pass classes via classes argument (not overriding attributes)
+    builder = HTMLBuilder(classes=expected_classes)
+    assert builder.attributes == expected_attributes
+    assert builder.attributes.classes == expected_classes
+    assert builder.classes == expected_classes
+
+    # Pass classes via classes argument (overriding attributes)
+    passed_attributes = Attributes({"id": "test", "class": "class3 Class-4"})
+    expected_attributes = Attributes({"id": "test", "class": "class-1 class2"})
+    expected_classes = Classes("class 1", "class2")  # TODO: Remove
+    builder = HTMLBuilder(attributes=passed_attributes, classes=expected_classes)
+    assert builder.attributes == expected_attributes
+    assert builder.attributes.classes == expected_classes
+    assert builder.classes == expected_classes
+
+
+def test_html_builder_repr(builder: HTMLBuilder) -> None:
+    """Tests the __repr__ method of the HTMLBuilder class."""
+    expected = (
+        "HTMLBuilder(attributes="
+        "Attributes(attributes="
+        "OrderedDict([('id', 'test'), ('disabled', True)])"
+        "))"
+    )
+    assert repr(builder) == expected
